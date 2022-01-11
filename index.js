7ce2db3e420369bc7cfb907ceeccc9ca52c7a23d
Crispin R <crispinr003@gmail.com>
2022-01-12 01:01:18 +0530

    Fix variable error

--- index.js
+++ index.js
@@ -30,5 +30,5 @@ io.on("connection", (socket) => {
 });
 
 server.listen(process.env.PORT, () => {
-  console.log(`Server is listening on port ${PORT}...`);
+  console.log(`Server is listening on port ${process.env.PORT}...`);
 });
